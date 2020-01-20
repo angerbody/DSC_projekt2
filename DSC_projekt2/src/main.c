63e1c5e91d8dd29ba3c4f183b3db31cbe7ee85cc
Konrad <Konrad@DESKTOP-AGSB3CE>
2020-01-20 16:12:20 +0100

    bez polskich znakow, dodane komentarze, usuniete zbedne linie

--- DSC_projekt2/src/main.c
+++ DSC_projekt2/src/main.c
@@ -20,29 +20,27 @@ int main(void)
 	//GPIO - PA8
 	RCC -> AHB2ENR |= (RCC_AHB2ENR_GPIOAEN);
 	GPIOA -> MODER &= ~(GPIO_MODER_MODER8_0);
-	GPIOA -> OSPEEDR |= (GPIO_OSPEEDR_OSPEED8_1) | (GPIO_OSPEEDR_OSPEED8_0);
+	GPIOA -> OSPEEDR |= (GPIO_OSPEEDR_OSPEED8_1) | (GPIO_OSPEEDR_OSPEED8_0); //High speed port
 
 	//RCC
-	//Konfiguracja MCO - sygna³  dzielony przez 2, Ÿród³o MSI
+	//Konfiguracja MCO - sygnal  dzielony przez 2, Ÿród³o MSI
 	//RCC -> CFGR |= (RCC_CFGR_MCOPRE_DIV2);
 	RCC -> CFGR |= (RCC_CFGR_MCOSEL_1); //MSI on MCO
 
 	//MSI 2MHz
-	//Czêstotliwoœc pracy MSI ustawiona na 2MHz
+	//Czestotliwosc pracy MSI ustawiona na 2MHz
 	RCC -> CR &= ~(0x000000F0); //RESET RANGE
 	RCC -> CR |= (RCC_CR_MSIRANGE_5);
 	RCC -> CR |= (RCC_CR_MSIRGSEL);
 
 	//HSI16
-	//W³¹czenie sygna³u HSI i ustawienie go jako SYSCLK
+	//Wlaczenie sygna³u HSI i ustawienie go jako SYSCLK
 	RCC -> CR |= (RCC_CR_HSION);
 	RCC -> CFGR |= (RCC_CFGR_SW_HSI);
 	RCC -> CFGR |= (RCC_CFGR_MCOSEL_0) | (RCC_CFGR_MCOSEL_1); //HSI on MCO
 
 	//PLL
-	//Konfiguracja PLL do pracy z maksymaln¹ czêstotliwoœci¹
-
-
+	//Konfiguracja PLL do pracy z maksymalna czestotliwoscia
 	RCC -> CR &= ~(RCC_CR_PLLON);
 	while ((RCC -> CR & ~(RCC_CR_PLLRDY_Msk)) == 0) {}
 
@@ -54,11 +52,11 @@ int main(void)
 	RCC -> CR |= (RCC_CR_PLLON);
 	RCC -> PLLCFGR |= (RCC_PLLCFGR_PLLREN);
 
-
+	//Ustawienie 'wait state' na 4 cykle
 	FLASH->ACR |= (FLASH_ACR_LATENCY_4WS);
-	RCC -> CFGR |= (RCC_CFGR_SW_PLL);
+	RCC -> CFGR |= (RCC_CFGR_SW_PLL); //PLL jako SYSCLK, niemozliwe bez wait state'a
 
-	//Ustawienie dzielnika czêstotliwosci
+	//Ustawienie dzielnika czestotliwosci
 	RCC -> PLLCFGR &= ~(RCC_PLLCFGR_PLLM_1); // /4
 	//RCC -> PLLCFGR |= (RCC_PLLCFGR_PLLR_1);
 
@@ -66,8 +64,10 @@ int main(void)
 	RCC -> CFGR |= (RCC_CFGR_MCOSEL_2) | (RCC_CFGR_MCOSEL_0); //PLL on MCO
 
 	//LSE
+	//Wlaczenie PWR i wylaczenie ochrony przed zapisem
 	RCC-> APB1ENR1 |= RCC_APB1ENR1_PWREN;
 	PWR -> CR1 |= PWR_CR1_DBP;
+
 	RCC -> BDCR |= (RCC_BDCR_LSEON); //LSE ON
 	RCC -> BDCR |= (RCC_BDCR_LSCOSEL); //LSE as Low Speed Clock
 	RCC -> BDCR |= (RCC_BDCR_LSCOEN);
@@ -84,32 +84,33 @@ int main(void)
 		RCC -> CFGR &= ~(RCC_CFGR_MCOSEL_3);
 
 		//RCC
-		//Konfiguracja MCO - sygna³  dzielony przez 2, Ÿród³o MSI
-		//RCC -> CFGR |= (RCC_CFGR_MCOPRE_DIV2);
+		//Konfiguracja MCO - zrodlo MSI
 		RCC -> CR &= ~(0x000000F0);
 		RCC -> CR |= (RCC_CR_MSIRANGE_6);
-		RCC -> CR &= ~(RCC_CR_MSIPLLEN);
+		RCC -> CR &= ~(RCC_CR_MSIPLLEN);     //Wylaczenie poprawy MSI celem prezentacji sygnalu przed poprawa
 		RCC -> CFGR |= (RCC_CFGR_MCOSEL_1); //MSI on MCO
 		HAL_Delay(20000);
+
+		//Poprawa czestotliwosci MSI przy pomocy LSE
 		RCC -> CR |= (RCC_CR_MSIPLLEN);
 		HAL_Delay(20000);
 
 
 		//MSI 2MHz
-		//Czêstotliwoœc pracy MSI ustawiona na 2MHz
+		//Czêstotliwosc pracy MSI ustawiona na 2MHz
 		RCC -> CR &= ~(0x000000F0); //RESET RANGE
 		RCC -> CR |= (RCC_CR_MSIRANGE_5);
 		RCC -> CR |= (RCC_CR_MSIRGSEL);
 		HAL_Delay(10000);
 
 		//HSI16
-		//W³¹czenie sygna³u HSI i ustawienie go jako SYSCLK
+		//Wlaczenie sygnalu HSI
 		RCC -> CR |= (RCC_CR_HSION);
 		RCC -> CFGR |= (RCC_CFGR_MCOSEL_0) | (RCC_CFGR_MCOSEL_1); //HSI on MCO
 		HAL_Delay(10000);
 
 		//PLL
-		//Konfiguracja PLL do pracy z maksymaln¹ czêstotliwoœci¹
+		//Konfiguracja PLL do pracy z maksymalna czestotliwoscia
 		RCC -> CFGR &= ~(RCC_CFGR_MCOSEL_1);
 		RCC -> CFGR |= (RCC_CFGR_MCOSEL_2) | (RCC_CFGR_MCOSEL_0); //PLL on MCO
 		HAL_Delay(10000);
@@ -117,18 +118,6 @@ int main(void)
 		//LSE
 		RCC -> CFGR |= (RCC_CFGR_MCOSEL_1); //LSE on MCO
 		HAL_Delay(10000);
-
-		/*RCC -> CFGR |= (RCC_CFGR_MCOSEL_1); //MSI on MCO
-		HAL_Delay(12000);
-		RCC -> CFGR |= (RCC_CFGR_MCOSEL_0); //HSI on MCO
-		HAL_Delay(12000);
-		RCC -> CFGR &= ~(RCC_CFGR_MCOSEL_1);
-		RCC -> CFGR |= (RCC_CFGR_MCOSEL_2); //PLL on MCO
-		HAL_Delay(12000);
-		RCC -> CFGR |= (RCC_CFGR_MCOSEL_1); //LSE on MCO
-		HAL_Delay(12000);
-		RCC -> CFGR &= ~(RCC_CFGR_MCOSEL_0);
-		RCC -> CFGR &= ~(RCC_CFGR_MCOSEL_2);*/
 
 	}
 }
